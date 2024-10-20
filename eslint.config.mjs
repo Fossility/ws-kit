1aaf6005a83d25036e9fddd4949734659944bc70
Hixo <hixohixo32@gmail.com>
2024-10-20 12:20:31 +0200

    feat: add sonarjs eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import sonarjs from 'eslint-plugin-sonarjs';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -18,4 +19,10 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettier.recommended,
+  sonarjs.configs.recommended,
+  {
+    plugins: {
+      sonarjs,
+    },
+  },
 ];
