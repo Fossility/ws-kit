a7fffa87260077fd5a0566c264cb867272165f77
Hixo <hixohixo32@gmail.com>
2024-10-20 11:09:08 +0200

    fix: remove spread operator

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["coverage/", "public/", "dist/", "pnpm-lock.yaml", "pnpm-workspaces.yaml"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    ignores: [
+      'coverage/',
+      'public/',
+      'dist/',
+      'pnpm-lock.yaml',
+      'pnpm-workspaces.yaml',
+    ],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintPluginPrettier.recommended
+  eslintPluginPrettier.recommended,
 ];
