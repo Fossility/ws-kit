45a6d103b2daaa108c9f090fe0aa15fc0aca6172
Hixo <hixohixo32@gmail.com>
2024-10-21 09:05:38 +0200

    refactor: remove ignores section in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,11 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-import sonarjs from 'eslint-plugin-sonarjs';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import sonarjs from "eslint-plugin-sonarjs";
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  {
-    ignores: [
-      'coverage/',
-      'public/',
-      'dist/',
-      'pnpm-lock.yaml',
-      'pnpm-workspaces.yaml',
-    ],
-  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
